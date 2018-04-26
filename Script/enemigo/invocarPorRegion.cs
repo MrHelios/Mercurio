d75dc8eb0736d265c32f27ba447dfa068becc9bb
Luciano <lhitaliani@gmail.com>
2018-04-26 08:59:33 -0300

    Se agrego evento de invocacion por colision

---
+++ Script/enemigo/invocarPorRegion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invocarPorRegion : MonoBehaviour
+{
+    public GameObject enem;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.name.Equals("hero"))
+        {
+            enem.SetActive(true);
+            Destroy(gameObject.GetComponent<invocarPorRegion>());
+        }
+    }
+
+}
