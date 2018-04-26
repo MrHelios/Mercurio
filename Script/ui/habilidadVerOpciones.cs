3648962afc2d0fd40c02809a67383d70791296e1
Luciano <lhitaliani@gmail.com>
2018-04-26 07:25:10 -0300

    Se agrego canvas simple para salir del juego

---
+++ Script/ui/habilidadVerOpciones.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class habilidadVerOpciones : MonoBehaviour
+{
+    private GameObject config;
+    private bool estado;
+    private cooldown cd;    
+	
+	void Start ()
+    {
+        config = GameObject.Find("Canvas/Opciones").transform.GetChild(0).gameObject;
+        estado = false;
+        cd = new cooldown();
+        cd.setUltimaVez(-99f);
+        cd.setCooldown(0.25f);        
+	}
+
+    public void cambiarEstado()
+    {
+        estado = !estado;
+        config.SetActive(estado);
+        cd.setUltimaVez(Time.time);
+    }
+	
+	void FixedUpdate ()
+    {
+        if (cd.tiempoCompletado() && Input.GetKey(KeyCode.Escape))
+            cambiarEstado();
+	}
+}
---
+++ Script/ui/salirJuego.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class salirJuego : MonoBehaviour
+{
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
