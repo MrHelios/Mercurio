4ba22c1cfe57afb7499dbdb6d9f0aae67291e04a
Luciano <lhitaliani@gmail.com>
2018-04-29 16:15:31 -0300

    la cantidad de vida que tengas aparece en la ui

---
+++ Script/ui/controladorVidaUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class controladorVidaUI : MonoBehaviour
+{
+    private string direccion;
+    private GameObject panelvida;
+	
+	void Start ()
+    {
+        direccion = "Canvas/Vida/Panel";
+        panelvida = GameObject.Find(direccion);
+	}
+
+    public void armar(int v)
+    {
+        GameObject go = panelvida.transform.GetChild(0).gameObject;
+        for(int i=1; i<v; i++)
+        {
+            GameObject nuevo = Instantiate(go);
+            nuevo.transform.SetParent(panelvida.gameObject.transform);
+            nuevo.transform.position = new Vector2(go.transform.position.x + 30*i, go.transform.position.y);
+        }
+    }
+
+    public void ganarVida(int i)
+    {
+        
+        panelvida.transform.GetChild(i).GetComponent<Image>().enabled = true;
+    }
+
+    public void perderVida(int i)
+    {
+        if (i >= 0)
+            panelvida.transform.GetChild(i).GetComponent<Image>().enabled = false;
+    }
+    
+}
