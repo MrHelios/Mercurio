588ce8be0bc92904c1805e5ea09fd4a8e110c0d4
Luciano <lhitaliani@gmail.com>
2018-04-29 13:20:15 -0300

    Ahora se pueden cargar otras escenas

---
+++ Script/ui/nuevaEscena.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nuevaEscena : MonoBehaviour
+{
+    public int scene;
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+}
