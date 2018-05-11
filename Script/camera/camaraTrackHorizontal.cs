4053f26c231ed13b4017633544ef0a7a1c0cda53
Luciano <lhitaliani@gmail.com>
2018-05-11 13:47:02 -0300

    Implementar camara con movimiento Horizontal

---
+++ Script/camera/camaraTrackHorizontal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camaraTrackHorizontal : MonoBehaviour
+{
+    private GameObject hero;
+
+	
+	void Start ()
+    {
+        hero = GameObject.Find("hero");
+	}
+	
+	
+	void Update ()
+    {
+        float x = hero.transform.position.x;        
+        transform.position = new Vector3(x ,-1, -10);
+	}
+}
