54c4bb03ee98422eec6fd44ca4ffe198ea5ab1f0
Luciano <lhitaliani@gmail.com>
2018-05-12 19:48:09 -0300

    script para mantener el hero a una estructura

---
+++ Script/hero/ancladoConEstructura.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ancladoConEstructura : MonoBehaviour
+{
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log(collision.gameObject.GetComponent<Rigidbody2D>().velocity);
+            Vector2 v = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(v.x, GetComponent<movRepetitivo>().getVelocidad().y);
+        }
+    }
+}
