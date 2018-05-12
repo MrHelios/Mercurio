c650eeb07da2f4e29ed3a15793ce66905d257098
Luciano <lhitaliani@gmail.com>
2018-05-12 16:01:53 -0300

    Se mejoro el sistema de teletransporte

--- Script/hero/teletransportar.cs
+++ Script/hero/teletransportar.cs
@@ -9,11 +9,35 @@ public class teletransportar : MonoBehaviour
     {
         if(collision.tag == "Player")
         {
-            collision.transform.position = ubicacion.transform.position;
             camara.SetActive(true);
-
-            GameObject.Find("Camera").SetActive(false);
+            cambiarCamaras();
+            cambiarMundo(collision);
         }
+    }
+
+    private void cambiarMundo(Collider2D collision)
+    {        
+        if (ubicacion.transform.parent.name != "escenario" && !ubicacion.transform.parent.gameObject.activeSelf)
+        {
+            ubicacion.transform.parent.gameObject.SetActive(true);
+        }
+        else if(ubicacion.transform.parent.name == "escenario")
+        {            
+            if (GameObject.Find("esc_secret_1") != null)
+                GameObject.Find("esc_secret_1").SetActive(false);
+            if (GameObject.Find("esc_secret_2") != null)
+                GameObject.Find("esc_secret_2").SetActive(false);
+        }
+
+        collision.transform.position = ubicacion.transform.position;
+    }
+
+    private void cambiarCamaras()
+    {
+        GameObject go = GameObject.Find("Camaras");
+        for (int i = 0; go.transform.childCount > i; i++)
+            if(camara != go.transform.GetChild(i).gameObject)
+                go.transform.GetChild(i).gameObject.SetActive(false);
     }
 
 
