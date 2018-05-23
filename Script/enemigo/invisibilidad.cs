222a72df5adff3145efafb7b7994b54dbb50250b
Luciano <lhitaliani@gmail.com>
2018-05-22 22:03:49 -0300

    se agrego la habilidad invisibilidad

---
+++ Script/enemigo/invisibilidad.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class invisibilidad : habilidad
+{
+    private bool activo;
+
+	void Awake ()
+    {
+        activo = false;
+	}    
+
+    public override void activar()
+    {
+        activo = true;
+        efecto();
+    }
+
+    public override void desactivar()
+    {
+        activo = false;
+        efecto();
+    }
+
+    protected override void efecto()
+    {
+        GetComponent<Animator>().enabled = !activo;
+        GetComponent<Collider2D>().enabled = !activo;
+        for(int i=0; i<transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<SpriteRenderer>().enabled = !activo;
+        }
+    }
+    
+}
