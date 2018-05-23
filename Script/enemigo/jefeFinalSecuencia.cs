200c5527c86e1d77527f6f267424fb932f15f082
Luciano <lhitaliani@gmail.com>
2018-05-22 23:08:06 -0300

    se agrego una clase abstracta jefefinalSecuencia

---
+++ Script/enemigo/jefeFinalSecuencia.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public abstract class jefeFinalSecuencia : MonoBehaviour
+{
+    protected habilidad[] habilidades;
+
+    protected void setHabilidad(int i, habilidad h)
+    {
+        habilidades[i] = h;
+    }
+
+    protected habilidad getHabilidad(int i)
+    {
+        return habilidades[i % habilidades.Length];
+    }
+
+}
