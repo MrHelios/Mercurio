af7b84e18e49da8107724be98bc55234763dc2e7
Luciano <lhitaliani@gmail.com>
2018-05-22 23:09:46 -0300

    se agrego clase jefe final 1-1b

---
+++ Script/enemigo/Jefe Finales/JF11b.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class JF11b : jefeFinalSecuencia
+{
+    private int contar;
+    private cooldown cd;
+
+    private int posicion;
+    public GameObject puntos;
+	
+	void Start ()
+    {
+        contar = 0;
+
+        posicion = 0;
+        cambiarPosicion();
+
+        habilidades = new habilidad[2];
+        setHabilidad(0, GetComponent<fantasmaTirarBomba>());
+        setHabilidad(1, GetComponent<invisibilidad>());
+
+        getHabilidad(contar).activar();
+
+        cd = new cooldown();
+        cd.setCooldown(5f);
+        cd.setUltimaVez(Time.time);
+    }
+
+    private void cambiarPosicion()
+    {
+        transform.position = puntos.transform.GetChild(posicion % puntos.transform.childCount).transform.position;
+        posicion++;
+    }
+	
+	void FixedUpdate ()
+    {
+	    if(cd.tiempoCompletado())
+        {
+            getHabilidad(contar).desactivar();
+            if (contar % 2 != 0)
+                cambiarPosicion();
+
+            contar++;
+            getHabilidad(contar).activar();
+
+            cd.setUltimaVez(Time.time);
+        }	
+	}
+}
