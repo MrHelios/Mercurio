8884bb62499230ef8323d6b9e3ea820c575eb77f
Luciano <lhitaliani@gmail.com>
2018-05-22 22:06:52 -0300

    se agrego el script para crearEnemigos

---
+++ Script/enemigo/crearEnemigo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class crearEnemigo : MonoBehaviour
+{
+    private bool crear;    
+
+    private void Start()
+    {
+        crear = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && crear)
+        {            
+            if (GetComponent<destruir>() != null)
+            {
+                GetComponent<destruir>().enabled = true;
+                for(int i=0; i<transform.childCount; i++)
+                {
+                    transform.GetChild(i).gameObject.SetActive(true);
+                }
+            }
+            else
+                Debug.Log(transform.name + ": No tiene la componente destruir.");
+            crear = false;
+        }
+    }
+
+}
