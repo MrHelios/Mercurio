c0d987222a3e6052d90b3f3166bc19c0c66e776c
Luciano <lhitaliani@gmail.com>
2018-05-24 21:45:53 -0300

    BUG: el hero recibe daño aun si el enemigo esta muerto

--- Script/enemigo/heroColision.cs
+++ Script/enemigo/heroColision.cs
@@ -2,13 +2,32 @@
 
 public class heroColision : MonoBehaviour
 {
+    public jefe vidaJefe; 
+    private bool activo;
+
+    void Start()
+    {
+        activo = true;    
+    }
+
+    public void activar()
+    {
+        activo = true;
+    }
+
+    public void desactivar()
+    {
+        activo = false;
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if(collision.tag == "Player" && activo)
         {
-            collision.gameObject.GetComponent<hero>().pierdeVida();
-            
+            if(vidaJefe != null && vidaJefe.estaVivo())
+                collision.gameObject.GetComponent<hero>().pierdeVida();
+            else if(vidaJefe == null)
+                collision.gameObject.GetComponent<hero>().pierdeVida();
         }        
     }
 
