3bcf582cc0cca1c4017783e323fc17d2613c9330
Luciano <lhitaliani@gmail.com>
2018-05-29 16:12:21 -0300

    se creo el controlador de municion para la ui

---
+++ Script/ui/controladorMunicionUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class controladorMunicionUI : MonoBehaviour
+{
+    private GameObject texto_ui;
+
+    private void Start()
+    {
+        texto_ui = GameObject.Find("Canvas/Municion/Panel/Text");
+        if (texto_ui == null)
+            Debug.Log("No hay Texto para las municiones");
+    }
+
+    public void cambiarTexto(int i)
+    {
+        texto_ui.GetComponent<Text>().text = "" + i;
+    }	
+	
+}
