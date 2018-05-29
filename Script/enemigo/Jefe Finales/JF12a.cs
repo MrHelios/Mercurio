d9822cd7de4bcc4ed93437e6fe4bb36eaa77e966
Luciano <lhitaliani@gmail.com>
2018-05-29 16:11:11 -0300

    se creo el script para jefe final 1-2a

---
+++ Script/enemigo/Jefe Finales/JF12a.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class JF12a : MonoBehaviour
+{
+    private cooldown cd;    
+    private float tiempo;
+    private int cooldown, posicion;
+
+    public GameObject[] enem;
+    public GameObject[] objetos;
+
+    void Start()
+    {
+        posicion = 0;
+        cooldown = 5;
+
+        cd = new cooldown();
+        cd.setCooldown(cooldown);
+        cd.setUltimaVez(-99f);
+
+        activarZona();
+    }    
+
+    private void activarZona()
+    {
+        tiempo = Time.time + 20f;        
+    }
+
+    private void desactivarZona()
+    {
+        for(int i=0; i<objetos.Length; i++)
+        {
+            objetos[i].SetActive(true);
+        }
+        Destroy(gameObject);
+    }
+
+    public void efecto()
+    {        
+        GameObject nuevo = Instantiate(enem[posicion % enem.Length]);
+        nuevo.SetActive(true);
+        nuevo.AddComponent<destruir>();
+
+        posicion++;
+    }
+
+    void FixedUpdate()
+    {
+        if (cd.tiempoCompletado())
+        {
+            efecto();
+            cd.setUltimaVez(Time.time);
+        }
+        else if (Time.time > tiempo)
+        {
+            desactivarZona();
+        }
+    }
+}
