1e5caf1aa5756930f890889897d2101d8a81a02f
Luciano <lhitaliani@gmail.com>
2018-05-29 15:56:40 -0300

    el script atqbasico ahora inicia el proyectil mas bajo si el jugador esta agachado

--- Script/hero/atqbasico.cs
+++ Script/hero/atqbasico.cs
@@ -16,7 +16,7 @@ public class atqbasico : atqDistancia
 
         hab_ejec = new cooldown();
         hab_ejec.setUltimaVez(-99f);
-        hab_ejec.setCooldown(1f);
+        hab_ejec.setCooldown(0.5f);
 
         seleccionarProyectil();
     }
@@ -30,12 +30,23 @@ public class atqbasico : atqDistancia
             Debug.Log("No ha sido seleccionado el proyectil para atqbasico.");
     }
 
+    private bool estaAgachado()
+    {
+        return GetComponent<agacharse>().getEstaAgachado();
+    }
+
     public override void efecto()
     {
         if(disp != null)
         {
             GameObject nuevo = Instantiate(disp);
-            nuevo.transform.position = disp.transform.position;
+            if (estaAgachado())
+            {
+                Vector3 v = new Vector3(disp.transform.position.x, disp.transform.position.y-0.5f, disp.transform.position.z);
+                nuevo.transform.position = v;
+            }                
+            else
+                nuevo.transform.position = disp.transform.position;
             nuevo.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
             nuevo.AddComponent<Rigidbody2D>();
             nuevo.GetComponent<Rigidbody2D>().gravityScale = 0;
