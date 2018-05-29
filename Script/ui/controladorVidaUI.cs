c97a9cd5faa166201ac459b1a51b0a8b75d70a60
Luciano <lhitaliani@gmail.com>
2018-05-29 15:50:19 -0300

    el script controladorvidaUI cambia de color si estas en estado invulnerable

--- Script/ui/controladorVidaUI.cs
+++ Script/ui/controladorVidaUI.cs
@@ -5,12 +5,46 @@ public class controladorVidaUI : MonoBehaviour
 {
     private string direccion;
     private GameObject panelvida;
+    private int[] rojo, celeste;
 	
 	void Awake ()
     {
         direccion = "Canvas/Vida/Panel";
         panelvida = GameObject.Find(direccion);
-	}
+
+        //rojo = [255,25,0];
+        rojo = new int[3];
+        rojo[0] = 255;
+        rojo[1] = 0;
+        rojo[2] = 0;
+
+        //celeste = [0,223,255];
+        celeste = new int[3];
+        celeste[0] = 0;
+        celeste[1] = 223;
+        celeste[2] = 255;
+    }
+
+    void Start()
+    {
+        pintarRojo();    
+    }
+
+    public void pintarCeleste()
+    {
+        for(int i=0; i<panelvida.transform.childCount; i++)
+        {
+            panelvida.transform.GetChild(i).GetComponent<Image>().color = new Color(celeste[0], celeste[1], celeste[2]);
+        }
+    }
+
+    public void pintarRojo()
+    {
+        for (int i = 0; i < panelvida.transform.childCount; i++)
+        {
+            panelvida.transform.GetChild(i).GetComponent<Image>().color = new Color(rojo[0], rojo[1], rojo[2]);
+        }
+    }
 
     public void armar(int v)
     {
