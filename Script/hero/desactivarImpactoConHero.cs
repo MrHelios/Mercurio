9c93af9881749d768edce45dba331a606d5ff5e6
Luciano <lhitaliani@gmail.com>
2018-05-29 16:04:30 -0300

    se creo un recolector de energia arcana: desactivarImpactoConHero

---
+++ Script/hero/desactivarImpactoConHero.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class desactivarImpactoConHero : MonoBehaviour
+{
+    private bool usado;
+
+    private void Start()
+    {
+        usado = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && !usado)
+        {
+            usado = true;
+            desactivar();
+        }
+    }
+
+    private void desactivar()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<destruir>().activar();
+        GameObject.Find("hero").GetComponent<municionArcana>().aumentarCarga();
+        Destroy(this);
+    }
+
+}
