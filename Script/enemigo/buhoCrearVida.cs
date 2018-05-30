14a6810cd94bf55756233d7f1792702abb94a559
Luciano <lhitaliani@gmail.com>
2018-05-30 12:28:08 -0300

    se creo la habilidad buhocrearvida

---
+++ Script/enemigo/buhoCrearVida.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class buhoCrearVida : habilidad
+{
+    public GameObject vida;
+    private bool estado;
+
+    private void Start()
+    {
+        activar();
+    }
+
+    public override void activar()
+    {
+        estado = true;
+    }
+
+    public override void desactivar()
+    {
+        estado = false;
+    }
+
+    protected override void efecto()
+    {
+        if (vida != null && estado)
+        {
+            vida.SetActive(true);
+            vida.AddComponent<Rigidbody2D>();
+            transform.parent.gameObject.GetComponent<destruir>().enabled = true;
+        }
+        else if(estado)
+            Debug.Log("Error: No se eligio vida.");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name.Contains("disp_basico"))
+        {
+            efecto();
+        }
+    }
+
+}
