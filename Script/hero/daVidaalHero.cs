e18a76c710ca94929fef573439bbfc8e502401bc
Luciano <lhitaliani@gmail.com>
2018-05-30 12:32:26 -0300

    se agrego la habilidad dar vida al hero

---
+++ Script/hero/daVidaalHero.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class daVidaalHero : habilidad
+{
+    private bool activado;
+
+    private void Start()
+    {
+        activar();
+    }
+
+    public override void activar()
+    {
+        activado = true;
+    }
+
+    public override void desactivar()
+    {
+        activado = false;
+    }
+
+    protected override void efecto()
+    {
+        GameObject.Find("hero").GetComponent<hero>().ganaVida();
+        GetComponent<SpriteRenderer>().enabled = false;
+        activado = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && activado)
+        {
+            efecto();
+        }
+    }
+
+}
