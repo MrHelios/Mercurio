39c703a095ca22e177576bbb076dc75c1ec82f8c
Luciano <lhitaliani@gmail.com>
2018-06-03 19:04:18 -0300

    se creo un controlador para activar y desactivar ui como efecto de algun evento

---
+++ Script/ui/controlarUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class controlarUI : MonoBehaviour
+{
+    public GameObject activar;
+    public GameObject desactivar;
+	
+	void Start ()
+    {
+        if (activar == null)
+            Debug.Log("No hay nada para activar en el controlador.");
+
+        if (desactivar == null)
+            Debug.Log("No hay nada para activar en el controlador.");
+    }
+
+    public void activarUI()
+    {
+        activar.SetActive(true);
+    }
+
+    public void desactivarUI()
+    {
+        desactivar.SetActive(false);
+    }
+
+    public void efecto()
+    {
+        activarUI();
+        desactivarUI();
+    }
+
+}
