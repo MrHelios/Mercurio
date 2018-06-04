33b00d562d38be11abac95d29f81da99c52c268f
Luciano <lhitaliani@gmail.com>
2018-06-04 15:31:38 -0300

    se creo la habilidad abrirCofre

---
+++ Script/hero/abrirCofre.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class abrirCofre : MonoBehaviour
+{
+    private bool estado;
+
+    void Start()
+    {
+        estado = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && Input.GetAxis("Fire4") > 0 && !estado)
+        {
+            estado = true;
+            transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            GetComponent<destruir>().enabled = true;
+        }
+    }
+
+}
